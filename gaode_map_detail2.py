29f7ca4c44768b9858cf58febafc8718fc1bf655
Albert <982675088@qq.com>
2017-08-26 18:54:31 +0800

    some changes by zq

--- gaode_map_detail2.py
+++ gaode_map_detail2.py
@@ -3,7 +3,10 @@
 import json
 import random
 import threading
+import traceback
 import urllib2
+from decimal import Decimal
+
 import MySQLdb
 import time
 import thread
@@ -11,11 +14,12 @@ import thread
 SAVE_TIME=''
 DISTX =[]
 DISTY =[]
+UPPER_X =[]
+UPPER_Y = []
+BOTTOM_X =[]
+BOTTOM_Y = []
 COUNT = 0
-proxies = [
-]
-time_lock='false'
-stop_lock='false'
+proxies = []
 threading_lock=threading.Lock()
 
 class MyThread(threading.Thread):
@@ -35,32 +39,40 @@ def address():
     global DISTY
     db = MySQLdb.connect(host='localhost',passwd='123aaaaaa',user='root',db='ofo',charset='utf8')
     cursor = db.cursor()
-    sql = 'SELECT * FROM putuo_address_info'
+    sql = 'SELECT * FROM putuo_upper_border_view'
     cursor.execute(sql)
     for row in cursor.fetchall():
-        DISTX.append(row[1])
-        DISTY.append(row[2])
+        UPPER_X.append(row[0])
+        UPPER_Y.append(row[1])
+    sql = 'SELECT * FROM putuo_bottom_border_view'
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        BOTTOM_X.append(row[0])
+        BOTTOM_Y.append(row[1])
+    interval = Decimal(0.0002)
+    for single_lng in xrange(0,len(BOTTOM_X)):
+        bottom_lat=BOTTOM_Y[single_lng]
+        upper_lat=UPPER_Y[single_lng]
+        for i in xrange(0,5):
+            cursorx=BOTTOM_X[single_lng]+interval*i
+            cursory=bottom_lat
+            while cursory<=upper_lat:
+                DISTX.append(cursorx.quantize(Decimal('0.000000')))
+                DISTY.append(cursory.quantize(Decimal('0.000000')))
+                cursory=cursory+interval
+        pass
     db.close()
-def address2():
-    global DISTX
-    global DISTY
-    for x in xrange(0,131):
-        distx=x*0.001+121.330
-        for y in xrange(0,101):
-            disty=y*0.001+31.220
-            DISTX.append(distx)
-            DISTY.append(disty)
+
 def link(bottom,upper):
     global DISTY
     global DISTX
     global COUNT
     global threading_lock
     for num in xrange(bottom,upper):
-        # 12321
-        if num<5248:
+        if num<len(DISTX):
             lon = DISTX[num]
             lat = DISTY[num]
-            spider(lon,lat,60)
+            spider(lon,lat,20)
 def spider(lon,lat,num):
     global COUNT
     global proxies
@@ -75,14 +87,12 @@ def spider(lon,lat,num):
     random_proxy = random.choice(proxies)
     proxy_support = urllib2.ProxyHandler({"http":random_proxy})
     opener = urllib2.build_opener(proxy_support)
-    # urllib2.install_opener(opener)
     request = urllib2.Request(url, headers=headers)
     try:
-        # html = urllib2.urlopen(request,timeout=3)
-        html = opener.open(request,timeout=2)
-        info = html.read()
+        html=opener.open(request,timeout=2)
+        info=html.read()
         info_out=info
-        info = json.loads(info)
+        info=json.loads(info)
         bicycle = info['bicycle']
         message = bicycle['message']
         if message=='ok':
@@ -91,36 +101,26 @@ def spider(lon,lat,num):
             threading_lock.acquire()
             COUNT = COUNT + 1
             print COUNT
-            print info_out
             threading_lock.release()
             db = MySQLdb.connect(host='localhost', passwd='123aaaaaa', user='root', db='ofo', charset='utf8')
             for item in items:
                 cursor = db.cursor()
-                sql = "insert into putuo_mobike_address3 (distX,distY,bikeIds,source,save_time,flag) values (%s,%s,%s,%s,%s,'170825_1') on duplicate key update distY = %s"
+                sql = "insert into putuo_mobike_address4 (distX,distY,bikeIds,source,save_time,flag) values (%s,%s,%s,%s,%s,'170826_2') on duplicate key update distY = %s"
                 param = (str(item['x']), str(item['y']),str(item['id']),str(item['source']),str(SAVE_TIME),str(item['y']))
                 cursor.execute(sql, param)
                 db.commit()
             db.close()
-        else:
-            pass
     except Exception as ex:
-        print str(Exception)
-        print ex.message
+        print ("again")
         if(num>0):
-            print ("again")
             num=num-1
             if len(proxies)>3:
                 threading_lock.acquire()
-                print ('proxy_num:'+str(len(proxies)))
                 if proxies.count(random_proxy)>0:
                     proxies.remove(random_proxy)
                 threading_lock.release()
             spider(lon,lat,num)
-        else:
-            threading_lock.acquire()
-            COUNT = COUNT + 1
-            print "die"
-            threading_lock.release()
+
 def getProxy():
     global proxies
     url='http://dev.kuaidaili.com/api/getproxy/?orderid=990355466199475&num=100&b_pcchrome=1&b_pcie=1&b_pcff=1&protocol=1&method=2&an_an=1&an_ha=1&sp1=1&sep=4'
@@ -140,102 +140,34 @@ def getProxy():
     for proxy in proxies:
         proxies_temp.append(proxy)
     proxies=proxies_temp
-def time_start_lock():
-    global time_lock
-    time_lock='true'
-    time.sleep(180)
-    time_lock='false'
-def time_stop_lock():
-    global stop_lock
-    time.sleep(3660)
-    stop_lock='true'
 
 if __name__ == '__main__':
-    thread.start_new_thread(time_stop_lock, ())
     address()
+    print ("import address successfully. Now start spider... ")
     threads_1=[]
-    threads_2=[]
-    threads_3=[]
-    threads_4=[]
-    threads_5=[]
-    threads_6=[]
     try:
-        while 1:
-            SAVE_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            thread.start_new_thread(time_start_lock ,())
-            COUNT=0
-            getProxy()
-            for num in xrange(0, 128):
-                start_num = num * 41
-                stop_num = num * 41 + 41
-                myThread = MyThread(start_num, stop_num)
-                if num < 43:
-                    threads_1.append(myThread)
-                elif num < 86:
-                    threads_2.append(myThread)
-                elif num < 128:
-                    threads_3.append(myThread)
-                # elif num < 111:
-                #     threads_4.append(myThread)
-                # elif num < 122:
-                #     threads_5.append(myThread)
-                else:
-                    threads_6.append(myThread)
+        SAVE_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        COUNT=0
+        getProxy()
+        for threadCount in xrange(0,71):
+            start_thread=threadCount*2000
+            for num in xrange(0,40):
+                start_num=start_thread+num*50
+                stop_num = start_thread+num*50+50
+                myThread = MyThread(start_num,stop_num)
+                threads_1.append(myThread)
             for t in threads_1:
                 t.start()
             for t in threads_1:
                 t.join()
-            while len(threads_1)!=0:
+            while len(threads_1) != 0:
                 threads_1.pop()
-            time.sleep(1)
-            getProxy()
+            print ("number of proxies saved:")
             print len(proxies)
-            for t in threads_2:
-                t.start()
-            for t in threads_2:
-                t.join()
-            while len(threads_2)!=0:
-                threads_2.pop()
-            time.sleep(1)
+            print ("Now sleep 5s...")
+            time.sleep(2)
             getProxy()
-            for t in threads_3:
-                t.start()
-            for t in threads_3:
-                t.join()
-            while len(threads_3)!=0:
-                threads_3.pop()
-            localtime = time.asctime(time.localtime(time.time()))
-            print "本地时间为 :", localtime
-            # time.sleep(2)
-            # getProxy()
-            # for t in threads_4:
-            #     t.start()
-            # for t in threads_4:
-            #     t.join()
-            # while len(threads_4)!=0:
-            #     threads_4.pop()
-            # time.sleep(2)
-            # getProxy()
-            # for t in threads_5:
-            #     t.start()
-            # for t in threads_5:
-            #     t.join()
-            # while len(threads_5)!=0:
-            #     threads_5.pop()
-            # getProxy()
-            # for t in threads_6:
-            #     t.start()
-            # for t in threads_6:
-            #     t.join()
-            # while len(threads_6)!=0:
-            #     threads_6.pop()
-            while 1:
-                time.sleep(1)
-                print (time_lock=='false')
-                if time_lock=='false':
-                    break
-            if stop_lock=='true':
-                break
-            pass
+        localtime = time.asctime(time.localtime(time.time()))
+        print "本地时间为 :", localtime
     except Exception as ex:
         print ex.message
