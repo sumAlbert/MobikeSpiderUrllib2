5123b9abe91c4356bca3e0752c0416b8552301f7
Albert <982675088@qq.com>
2017-08-27 12:58:33 +0800

    some changes by zq

--- gaode_map_detail2.py
+++ gaode_map_detail2.py
@@ -4,6 +4,7 @@ import json
 import random
 import threading
 import traceback
+import urllib
 import urllib2
 from decimal import Decimal
 
@@ -20,6 +21,8 @@ BOTTOM_X =[]
 BOTTOM_Y = []
 COUNT = 0
 proxies = []
+time_lock='false'
+stop_lock='false'
 threading_lock=threading.Lock()
 
 class MyThread(threading.Thread):
@@ -49,7 +52,7 @@ def address():
     for row in cursor.fetchall():
         BOTTOM_X.append(row[0])
         BOTTOM_Y.append(row[1])
-    interval = Decimal(0.001)
+    interval = Decimal(0.0002)
     for single_lng in xrange(0,len(BOTTOM_X)):
         bottom_lat=BOTTOM_Y[single_lng]
         upper_lat=UPPER_Y[single_lng]
@@ -62,7 +65,15 @@ def address():
                 cursory=cursory+interval
         pass
     db.close()
-
+def time_start_lock():
+    global time_lock
+    time_lock='true'
+    time.sleep(1)
+    time_lock='false'
+def time_stop_lock():
+    global stop_lock
+    time.sleep(1)
+    stop_lock='true'
 def link(bottom,upper):
     global DISTY
     global DISTX
@@ -77,42 +88,47 @@ def spider(lon,lat,num):
     global COUNT
     global proxies
     global SAVE_TIME
-    url ='http://m5.amap.com/ws/shield/bicycle_status?source=all&language=zh_CN&div=IOSH080102&client_network_class=4&tid=WY6TQ084yVIDAKWNjcm31Ssq&cmd=2&stepid=264&channel=amap7a&appstartid=208705228997&diu2=35F0171F-4627-4183-83AC-EA5092F5DF65&spm=06171318008672&city=021&version=2&session=208705162&cifa=80027004c00d080750f4b101010000000000000000000000000000000000000000000000000000000000000a000000060031302e332e3309006950686f6e65382c3405004150504c450a00382e312e322e32313237000000000000000000001e00&x='+str(lon)+'&y='+str(lat)+'&dip=10920&diu3=3e2a8f227278ca02f2c54d768f8184a449bcbe11&diu=64DA9658-07E5-45EA-AE09-888BB7518A56&aetraffic=9&dic=C3320&dibv=2127&frm=amap&adiu=8eihnid9b799dhhphplfpeb45d2cc6&output=json&sign=0F845F6ED19CC9686D6289AA45DD5DC6&csid=8870CC7B-9D50-499A-8722-19665F20EE33'
+    url ='https://api.open.ofo.com/v1/near/bicycle'
     headers = {
-        'host':'m5.amap.com',
-        'user-agent':'iphone OS 10.3.3',
-        'connection': 'close',
-        'accept - encoding': 'gzip'
+        'Host': 'api.open.ofo.com',
+        'Content - Type': 'application / json',
+        'Accept': '* / *',
+        'Accept - Encoding': 'gzip, deflate',
+        'Connection': 'keep - alive',
+        'Pragma': 'no - cache',
+        'User - Agent': 'OneTravel/5.1.8 (iPhone; iOS 10.3.3; Scale/2.00)',
+        'Accept - Language': 'zh - Hans - CN;q = 1',
+        'Cache - Control': 'no - cache'
+
     }
+    values={"appKey":"didi","number":"20","longitude":lon,"radius":"200","mapType":"1","latitude":lat,"datatype":"101","appversion":"5.1.8"}
+    data = urllib.urlencode(values)
     random_proxy = random.choice(proxies)
     proxy_support = urllib2.ProxyHandler({"http":random_proxy})
     opener = urllib2.build_opener(proxy_support)
-    request = urllib2.Request(url, headers=headers)
+    request = urllib2.Request(url, headers=headers,data=data)
     try:
         html=opener.open(request,timeout=2)
         info=html.read()
-        print info
-        info_out=info
+        infoout=info
         info=json.loads(info)
-        bicycle = info['bicycle']
-        message = bicycle['message']
-        if message=='ok':
-            bicycles = bicycle['bicycles']
-            items = bicycles['item']
-            threading_lock.acquire()
-            COUNT = COUNT + 1
-            # print COUNT
-            threading_lock.release()
-            db = MySQLdb.connect(host='localhost', passwd='123aaaaaa', user='root', db='ofo', charset='utf8')
-            for item in items:
-                cursor = db.cursor()
-                sql = "insert into putuo_mobike_address4 (distX,distY,bikeIds,source,save_time,flag) values (%s,%s,%s,%s,%s,'170826_4') on duplicate key update distY = %s"
-                param = (str(item['x']), str(item['y']),str(item['id']),str(item['source']),str(SAVE_TIME),str(item['y']))
-                cursor.execute(sql, param)
-                db.commit()
-            db.close()
+        bicycle = info['body']
+        items = bicycle['bicycles']
+        threading_lock.acquire()
+        COUNT = COUNT + 1
+        print COUNT
+        print infoout
+        threading_lock.release()
+        db = MySQLdb.connect(host='localhost', passwd='123aaaaaa', user='root', db='ofo', charset='utf8')
+        for item in items:
+            cursor = db.cursor()
+            sql = "insert into putuo_mobike_address3 (distX,distY,bikeIds,source,save_time,flag) values (%s,%s,%s,'ofo',%s,'170827_5') on duplicate key update distY = %s"
+            param = (str(item['longitude']), str(item['latitude']),str(item['bicycleNo']),str(SAVE_TIME),str(item['latitude']))
+            cursor.execute(sql, param)
+            db.commit()
+        db.close()
     except Exception as ex:
-        # print ("again")
+        print ("again")
         if(num>0):
             num=num-1
             if len(proxies)>3:
@@ -145,29 +161,41 @@ def getProxy():
 if __name__ == '__main__':
     address()
     print ("import address successfully. Now start spider... ")
+    thread.start_new_thread(time_stop_lock, ())
     threads_1=[]
     try:
-        SAVE_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         COUNT=0
         getProxy()
-        for threadCount in xrange(0,71):
-            start_thread=threadCount*2000
-            for num in xrange(0,40):
-                start_num=start_thread+num*50
-                stop_num = start_thread+num*50+50
-                myThread = MyThread(start_num,stop_num)
-                threads_1.append(myThread)
-            for t in threads_1:
-                t.start()
-            for t in threads_1:
-                t.join()
-            while len(threads_1) != 0:
-                threads_1.pop()
-            print ("number of proxies saved:")
-            print len(proxies)
-            print ("Now sleep 5s...")
-            time.sleep(2)
-            getProxy()
+        while 1:
+            SAVE_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            thread.start_new_thread(time_start_lock ,())
+            for threadCount in xrange(0,48):
+                start_thread=threadCount*3000
+                for num in xrange(0,60):
+                    start_num=start_thread+num*50
+                    stop_num = start_thread+num*50+50
+                    myThread = MyThread(start_num,stop_num)
+                    threads_1.append(myThread)
+                for t in threads_1:
+                    t.start()
+                for t in threads_1:
+                    t.join()
+                while len(threads_1) != 0:
+                    threads_1.pop()
+                print ("number of proxies saved:")
+                print len(proxies)
+                print ("Now sleep 5s...")
+                time.sleep(5)
+                getProxy()
+            while 1:
+                time.sleep(1)
+                print (time_lock == 'false')
+                if time_lock == 'false':
+                    COUNT=0
+                    break
+            if stop_lock == 'true':
+                break
+            pass
         localtime = time.asctime(time.localtime(time.time()))
         print "本地时间为 :", localtime
     except Exception as ex:
